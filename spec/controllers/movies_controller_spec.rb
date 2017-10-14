fdc89fbb390a334df53d0e23e4d01ba6aadfc0e9
LeiXiang <lei-xiang@uiowa.edu>
2017-10-14 23:18:03 +0000

    message of your choosing

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -19,6 +19,74 @@ describe MoviesController do
       allow(Movie).to receive(:find_in_tmdb).and_return (fake_results)
       post :search_tmdb, {:search_terms => 'Ted'}
       expect(assigns(:movies)).to eq(fake_results)
+    end
+    it 'should redirect to movies_path if the movie is not in tmdb' do
+      fake_results = []
+      expect(Movie).to receive(:find_in_tmdb).with('Ted').
+        and_return(fake_results)
+      post :search_tmdb, {:search_terms => 'Ted'}
+      expect(response).to redirect_to('/movies')
+    end
+    it 'should redirect to movies if empty key words' do
+      post :search_tmdb, {:search_terms => ' '}
+      expect(response).to redirect_to('/movies')
+    end
+    it 'should check for invalid search terms then notify user' do
+      fake_results = [double('Movie'), double('Movie')]
+      allow(Movie).to receive(:find_in_tmdb).and_return (fake_results)
+      post :search_tmdb, {:search_terms => ''}
+      expect(flash[:notice]).to eq("Invalid search term")
     end 
+    it 'should redirect the user if invalid search' do
+      fake_results = [double('Movie'), double('Movie')]
+      allow(Movie).to receive(:find_in_tmdb).and_return (fake_results)
+      post :search_tmdb, {:search_terms => ''}
+      expect(response).to redirect_to(movies_path)
+    end
+    it 'should check for no match from search then notify user' do
+      fake_results = []
+      allow(Movie).to receive(:find_in_tmdb).and_return (fake_results)
+      post :search_tmdb, {:search_terms => 'movie title with no match'}
+      expect(flash[:notice]).to eq("No matching movies were found on TMDb")
+    end
+    it 'should redirect the user if no match for search terms' do
+      fake_results = []
+      allow(Movie).to receive(:find_in_tmdb).and_return (fake_results)
+      post :search_tmdb, {:search_terms => 'Ted'}
+      expect(response).to redirect_to(movies_path)
+    end 
+    it 'should create two instance variables for communication with the view' do
+      fake_results = 'test'
+      allow(Movie).to receive(:find_in_tmdb).and_return (fake_results)
+      post :search_tmdb, {:search_terms => 'test'}
+      expect(assigns(:search_terms)).to eq 'test'
+      expect(assigns(:movies)).to eq 'test'
+    end
+  end
+  
+  describe 'Add movie from TMDb' do
+    it 'should call the model method that performs create_from_tmdb' do
+        Movie.should_receive(:create_from_tmdb).with('406234')
+        post :add_tmdb, {"utf8"=>"✓", "tmdb_movies"=>{"406234"=>"1"}, "commit"=>"Add Selected Movies", "id"=>"search_tmdb"}
+        # "checkbox"=>{"406234"=>"1"}, "commit"=>"Add Selected Movies", "id"=>"search_tmdb"
+        expect(flash[:notice]).to be_present
+    end
+    it 'should notify No movies selected and redirect to movies' do
+        post :add_tmdb, {"utf8"=>"✓", "tmdb_movies"=> nil, "commit"=>"Add Selected Movies", "id"=>"search_tmdb"}
+        expect(flash[:notice]).to eq "No movies selected"
+        expect(response).to redirect_to('/movies')
+    end
+    it 'should flash a message for successful addition' do
+      movie = {double("movie") => 0}
+      expect(Movie).to receive(:create_from_tmdb)
+      post :add_tmdb, {:tmdb_movies => movie}
+      expect(flash[:notice]).to eq 'Movies successfully added to Rotten Potatoes'
+      expect(response).to redirect_to(movies_path)
+    end
+    it 'should add all movies checked by user' do
+      movies = {"tmdb_movies" => {"203765"=>"1", "376659"=>"1", "9737"=>"1"}}
+      expect(Movie).to receive(:create_from_tmdb).exactly(3).times
+      post :add_tmdb, movies
+    end
   end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,11 +6,70 @@ describe Movie do
         expect( Tmdb::Movie).to receive(:find).with('Inception')
         Movie.find_in_tmdb('Inception')
       end
+    context 'ratings defined' do
+      it 'should have G PG PG-13 NC-17 R' do
+        expected_result = %w(G PG PG-13 NC-17 R)
+        result = Movie.all_ratings
+        expect(result).to eq(expected_result)
+      end
     end
+    context 'with matches' do
+      it "should render an array of hashes with two movies and if no rating then add Not rated" do
+          finds = [double("movie1"), double('movie2')]
+          releases = [{"countries" => [{"certification" => "", "iso_3166_1" => "US"}]}, {"countries" => [{"certification" => "PG", "iso_3166_1" => "US"}]}]
+          allow(finds[0]).to receive(:id).and_return('1')
+          allow(finds[0]).to receive(:title).and_return('first_title')
+          allow(finds[0]).to receive(:release_date).and_return('date1')
+          allow(finds[1]).to receive(:id).and_return('2')
+          allow(finds[1]).to receive(:title).and_return('second_title')
+          allow(finds[1]).to receive(:release_date).and_return('date2')
+          allow(Tmdb::Movie).to receive(:find).with('shrek').and_return(finds)
+          allow(Tmdb::Movie).to receive(:releases).with('1').and_return(releases[0])
+          allow(Tmdb::Movie).to receive(:releases).with('2').and_return(releases[1])
+          results = Movie.find_in_tmdb('shrek')
+          expect(results[0][:tmdb_id]).to eq('1')
+          expect(results[1][:tmdb_id]).to eq('2')
+          expect(results[0][:title]).to eq('first_title')
+          expect(results[1][:title]).to eq('second_title')
+          expect(results[0][:rating]).to eq('Not rated')
+          expect(results[1][:rating]).to eq('PG')
+          expect(results[0][:release_date]).to eq('date1')
+          expect(results[1][:release_date]).to eq('date2')
+        end
+      end
+    context 'with no matches' do
+        it 'should return nil when no keywords' do
+          results = Movie.find_in_tmdb('')
+          expect(results).to eq(nil)
+        end
+      end
+    end
+    
     context 'with invalid key' do
       it 'should raise InvalidKeyError if key is missing or invalid' do
         allow(Tmdb::Movie).to receive(:find).and_raise(Tmdb::InvalidApiKeyError)
         expect {Movie.find_in_tmdb('Inception') }.to raise_error(Movie::InvalidKeyError)
+      end
+    end
+    
+    context 'with valid key' do
+      it 'should call create from tmdb' do
+        allow(Tmdb::Movie).to receive(:detail).with(118)
+        expect {Movie.create_from_tmdb(118)}
+      end
+    end
+    
+    context 'with valid movie' do
+      it 'should add the movie to the database if no rating then add Not rated' do
+        movie_details = {"title" => "title", "release_date" => "date", "overview" => "description"}
+        releases = {"countries" => [{"certification" => "", "iso_3166_1" => "US"}]}
+        allow(Tmdb::Movie).to receive(:detail).with(0).and_return(movie_details)
+        allow(Tmdb::Movie).to receive(:releases).with(0).and_return(releases)
+        results = Movie.create_from_tmdb(0)
+        expect(results["title"]).to eq(movie_details["title"])
+        expect(results["release_date"]).to eq(movie_details["date"])
+        expect(results["description"]).to eq(movie_details["overview"])
+        expect(results["rating"]).to eq("Not rated")
       end
     end
   end
